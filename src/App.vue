d9d1dc5ba3c5693dd53edbb0456faee61c8ee886
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-09-23 10:05:02 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+// import NameComponent from "path.component";
+
+export default{
+   name: "App",
+   data () {
+      return {
+
+      }
+   },
+   components: {
+      // NameComponent,
+   }
+}
+
+</script>
+
+<template>
+
+
+
+</template>
+
+<style lang="scss">
+
+@use "./assets/scss/app.scss";
+
+</style>
