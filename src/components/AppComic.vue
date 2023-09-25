31eac7b75652d8863414b8889e0539b15ae6b152
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-09-25 18:09:31 +0200

    add comics series

---
+++ src/components/AppComic.vue
@@ -0,0 +1,20 @@
+<script>
+   export default{
+      name: "AppComic",
+      props: {
+         img: String,
+         name: String
+      }
+   }
+</script>
+
+<template>
+
+   <div class="col-2 mb-3 p-2">
+      <a href="#" class="comic-thumb mb-2">
+         <img :src="img" class="w-100">
+      </a>      
+      <a href="#" class="text-white text-uppercase comic-series">{{name}}</a>
+   </div>
+
+</template>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,14 +1,36 @@
 <script>
+import AppComic from './AppComic.vue';
+import comics from "../comics.js"
+
 export default{
    name: "AppMain",
-
+   data (){
+      return {
+         comics
+      }
+   },
+   components: {
+      AppComic,
+   }
 }
 </script>
 
 <template>
 
    <main>
-      <div class="text-white m-auto py-4 dc_max_width">Content goes here</div>
+      <div class="upper-main">
+         <div class="text-uppercase dc_max_width m-auto position-relative h-100">
+            <div class="badge-jumbotron">Current series</div>
+         </div>
+         
+      </div>
+      <div class="lower-main">
+         <div class="row dc_max_width m-auto py-5">
+
+            <AppComic v-for="comic in comics.comics" :name=comic.series :img=comic.thumb></AppComic>
+
+         </div>
+      </div>
    </main>
    
 
